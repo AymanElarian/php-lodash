67303a8c452e4aa38552fe9586d016afa5bdb103
Mohamed Meabed <mo.meabed@gmail.com>
2016-12-29 16:58:13 +0400

    Update namespaces

--- src/lodash.php
+++ src/lodash.php
@@ -1,7 +1,4 @@
 <?php
-
-namespace __;
-
 /*                                                 *\
  *                   lodash-php                    *
  *                                                 *
